cd9b8950bcde468627a1c8cc9cb891e8b9913bfe
unknown <anshumank1411@gmail.com>
2021-09-22 15:30:24 +0530

    Modified output in Forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=3106a53143dac9e5261da807ecc85c66&query=${latitude},${longitude}&units=f`
+    const url = `http://api.weatherstack.com/current?access_key=3106a53143dac9e5261da807ecc85c66&query=${latitude},${longitude}`
 
     request( {url, json: true}, (error, {body}) => {
 
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
                 callback('Unable to find the location', undefined)
             } 
             else {
-                callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+                callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`
                 )
             }
             
